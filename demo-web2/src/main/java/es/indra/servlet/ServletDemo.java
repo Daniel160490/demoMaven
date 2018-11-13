abc806ced366ce06ab8dc40601fba842908f3390
Daniel160490 <danigd71@gmail.com>
2018-11-13 11:31:25 +0100

    Ejemplo de Servlet

---
+++ demo-web2/src/main/java/es/indra/servlet/ServletDemo.java
@@ -0,0 +1,52 @@
+package es.indra.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletDemo
+ */
+@WebServlet("/ServletDemo")
+public class ServletDemo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletDemo() {
+        super();
+        
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		PrintWriter pw = response.getWriter();
+		pw.append("<h1> Bienvenido </h1>");
+		pw.append("<p> Hola </p>");
+		pw.append("<p> Daniel </p>");
+		
+		//pw.close();
+		//Redirige al index.jsp
+		request.getRequestDispatcher("index.jsp").forward(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
