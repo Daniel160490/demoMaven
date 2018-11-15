eb108def5b4bd94184c71c15cf7035a717c2ddb8
Daniel Garcia Diaz <daniel@MacBook-Air-de-Daniel.local>
2018-11-15 16:49:46 +0100

    Api para carrito de compra

---
+++ api-carro/src/main/java/es/indra/apicarro/ApiCarroApplication.java
@@ -0,0 +1,12 @@
+package es.indra.apicarro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCarroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCarroApplication.class, args);
+	}
+}
