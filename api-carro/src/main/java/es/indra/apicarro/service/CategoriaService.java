16f6d94249b886eabf29794c4be74333df4ab315
Daniel Garcia Diaz <daniel@MacBook-Air-de-Daniel.local>
2018-11-15 17:06:29 +0100

    Creando la api

--- api-carro/src/main/java/es/indra/apicarro/service/CategoriaService.java
+++ api-carro/src/main/java/es/indra/apicarro/service/CategoriaService.java
@@ -3,10 +3,23 @@
  */
 package es.indra.apicarro.service;
 
+import java.util.List;
+
+import es.indra.apicarro.model.Categoria;
+
 /**
  * @author daniel
  *
  */
 public interface CategoriaService {
 
+	public List<Categoria> getAllCategoria();
+
+	public Categoria getCategoriaById(int id);
+
+	public Categoria updateBlog(Categoria categoria);
+
+	public Categoria saveCategoria(Categoria categoria);
+
+	public void removeCategoria(Categoria categoria);
 }
--- api-carro/src/main/java/es/indra/apicarro/service/impl/CategoriaServiceImpl.java
+++ api-carro/src/main/java/es/indra/apicarro/service/impl/CategoriaServiceImpl.java
@@ -3,12 +3,63 @@
  */
 package es.indra.apicarro.service.impl;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import es.indra.apicarro.model.Categoria;
+import es.indra.apicarro.repository.CategoriaRepository;
 import es.indra.apicarro.service.CategoriaService;
 
 /**
  * @author daniel
  *
  */
+@Component
 public class CategoriaServiceImpl implements CategoriaService {
 
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	@Override
+	public List<Categoria> getAllCategoria() {
+
+		return null;
+	}
+
+	@Override
+	public Categoria getCategoriaById(int id) {
+		/*
+		 * Optional se usa por si lo que se esta buscando no está
+		 */
+		Optional<Categoria> cate = this.categoriaRepository.findById(id);
+		if (cate.isPresent()) {
+			return cate.get();
+
+		}
+		return null;
+	}
+
+	@Override
+	public Categoria updateBlog(Categoria categoria) {
+
+		if (this.getCategoriaById(categoria.getId()) != null) {
+			return this.categoriaRepository.save(categoria);
+		}
+
+		return null;
+	}
+
+	@Override
+	public Categoria saveCategoria(Categoria categoria) {
+
+		return this.categoriaRepository.save(categoria);
+	}
+
+	@Override
+	public void removeCategoria(Categoria categoria) {
+		this.categoriaRepository.delete(categoria);
+	}
 }
