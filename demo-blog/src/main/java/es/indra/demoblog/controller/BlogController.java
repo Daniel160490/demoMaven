ca28092c22d240afcc6eef3e2466477f2cf4ac40
Daniel Garcia Diaz <daniel@MacBook-Air-de-Daniel.local>
2018-11-15 12:33:36 +0100

    Nuevos cambios en blog

--- demo-blog/src/main/java/es/indra/demoblog/controller/BlogController.java
+++ demo-blog/src/main/java/es/indra/demoblog/controller/BlogController.java
@@ -3,18 +3,61 @@
  */
 package es.indra.demoblog.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import es.indra.demoblog.model.Blog;
 import es.indra.demoblog.service.BlogService;
 
 /**
+ * 
  * @author daniel
  *
  */
-@Controller
+@RestController
 public class BlogController {
 
 	@Autowired
-	BlogService blogServive;
+	BlogService blogService;
+
+	@RequestMapping(value = "/blog", method = RequestMethod.GET)
+	public ResponseEntity<List<Blog>> getAllBlog() {
+
+		List<Blog> todosLosBlogs = this.blogService.getAllBlog();
+		ResponseEntity<List<Blog>> response = new ResponseEntity<List<Blog>>(todosLosBlogs, HttpStatus.OK);
+
+		return response;
+	}
+
+	@RequestMapping(value = "/blog/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Blog> getToDoById(@PathVariable("id") int id) {
+
+		return new ResponseEntity<Blog>(blogService.getBlogById(id), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/blog", method = RequestMethod.POST)
+	public ResponseEntity<Blog> saveToDo(@RequestBody Blog blog) {
+		return new ResponseEntity<Blog>(blogService.saveBlog(blog), HttpStatus.OK);
+
+	}
+
+	@RequestMapping(value = "/blog", method = RequestMethod.PUT)
+	public ResponseEntity<Blog> updateToDo(@RequestBody Blog blog) {
+		return new ResponseEntity<Blog>(blogService.saveBlog(blog), HttpStatus.OK);
+
+	}
+
+	public ResponseEntity<String> removeToDoById(@PathVariable("id") int id) {
+		Blog blog = blogService.getBlogById(id);
+		blogService.removeBlog(blog);
+		return new ResponseEntity<String>("Blog eliminado", HttpStatus.OK);
+	}
 }
