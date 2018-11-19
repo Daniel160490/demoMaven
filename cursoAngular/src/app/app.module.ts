5ddd5588be60559f4028b4de5f29af3c972a7441
Daniel160490 <danigd71@gmail.com>
2018-11-19 17:48:24 +0100

    Formateo de pipe

--- cursoAngular/src/app/app.module.ts
+++ cursoAngular/src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Pipe } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MultiplicadorPipe } from './app.component';
 
 @NgModule({
   declarations: [
