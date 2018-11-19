784b6634bd8bb2a2bd3b5a707c8a59a2e2b9481b
Daniel160490 <danigd71@gmail.com>
2018-11-19 16:11:17 +0100

    Ejemplos con Angular

--- cursoAngular/src/app/app.component.ts
+++ cursoAngular/src/app/app.component.ts
@@ -1,15 +1,34 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: "app-root",
+  templateUrl: "./app.component.html",
   styles: []
 })
 export class AppComponent {
-  title = 'MisCursos';
+  title = "MisCursos";
   cursos = [
-    {'id': 1 , 'nombre': 'Angular 2', 'descripcion': 'Introducción al desarrollo avanzado en Js', 'duracion': 8},
-    {'id': 2 , 'nombre': 'NodeJS', 'descripcion': 'Introducción al server en Js', 'duracion': 4},
-    {'id': 3 , 'nombre': 'Angular 6', 'descripcion': 'Introducción a Angular', 'duracion': 120}];
+    {
+      id: 1,
+      nombre: "Angular 2",
+      descripcion: "Introducción al desarrollo avanzado en Js",
+      duracion: 8
+    },
+    {
+      id: 2,
+      nombre: "NodeJS",
+      descripcion: "Introducción al server en Js",
+      duracion: 4
+    },
+    {
+      id: 3,
+      nombre: "Angular 6",
+      descripcion: "Introducción a Angular",
+      duracion: 120
+    }
+  ];
 
+  seleccionar(curso) {
+    console.log(curso);
+  }
 }
