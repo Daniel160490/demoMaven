b5f8c8900c748479e362ca7ded9499929eb5acc9
Daniel Garcia Diaz <daniel@MacBook-Air-de-Daniel.local>
2018-11-20 11:44:32 +0100

    Añadiendo componentes

--- cursoAngular/src/app/multiplicador.pipe.ts
+++ cursoAngular/src/app/multiplicador.pipe.ts
@@ -1,9 +1,12 @@
-import { Pipe } from '@angular/core';
+import { Pipe , PipeTransform} from '@angular/core';
 @Pipe({ name: 'multiplicador' })
+
+
+
 export class MultiplicadorPipe implements PipeTransform {
   transform(valorRecibido: number, factor: number): number {
-  let fac = factor;
-  let mult = (valorRecibido * fac);
-  return mult;
+    let fac = factor;
+    let mult = (valorRecibido * fac);
+    return mult;
   }
-  }
+}
